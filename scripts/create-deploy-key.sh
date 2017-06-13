27130443a0650aee2f183b896f853165b6f69098
Alekseo <alekseyleonidovich@gmail.com>
2017-06-13 10:19:19 +0300

    drone init

--- scripts/create-deploy-key.sh
+++ scripts/create-deploy-key.sh
@@ -4,7 +4,7 @@
 # check .travis.yml there should be only one key
 # run this once for repository
 
-scp deploy@bot.nickvorobiov.com:/home/deploy/.ssh/id_rsa ./deploy-key
+
 chmod 600 ./deploy-key
 travis encrypt-file ./deploy-key --add
 rm ./deploy-key
